8d280226490dfb6ae8e859fb59e9d106a16b9ed4
huyvu2001 <41413347+huyvu2001@users.noreply.github.com>
2021-06-09 13:28:29 +0700

    photo picker added for posting recipes

--- flutter_application_1/lib/screens/post_recipe_screen.dart
+++ flutter_application_1/lib/screens/post_recipe_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/components/constaints.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PostRecipeScreen extends StatefulWidget {
   @override
@@ -8,6 +11,7 @@ class PostRecipeScreen extends StatefulWidget {
 
 class _PostRecipeScreenState extends State<PostRecipeScreen> {
   bool isBookmark = false;
+
   int currentTab = 0;
   final pageController = PageController();
   final listWidget = <Widget>[];
@@ -39,16 +43,11 @@ class _PostRecipeScreenState extends State<PostRecipeScreen> {
           backgroundColor: kPrimaryColor,
         ),
         resizeToAvoidBottomInset: false,
-        body: Column(children: [
-          SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height - 140,
-              child: PageView(
-                children: listWidget,
-                controller: pageController,
-                onPageChanged: (value) => setState(() => currentTab = value),
-              ))
-        ]),
+        body: PageView(
+          children: listWidget,
+          controller: pageController,
+          onPageChanged: (value) => setState(() => currentTab = value),
+        ),
         bottomNavigationBar: BottomNavigationBar(
           items: [
             BottomNavigationBarItem(
@@ -70,23 +69,106 @@ class _PostRecipeScreenState extends State<PostRecipeScreen> {
   }
 }
 
-class Introduction extends StatelessWidget {
+class Introduction extends StatefulWidget {
+  @override
+  _IntroductionState createState() => _IntroductionState();
+}
+
+class _IntroductionState extends State<Introduction> {
+  File _image;
+  Future _imgFromCamera() async {
+    File image = await ImagePicker.pickImage(
+        source: ImageSource.camera, imageQuality: 50);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  Future _imgFromGallery() async {
+    File image = await ImagePicker.pickImage(
+        source: ImageSource.gallery, imageQuality: 50);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  new ListTile(
+                      leading: new Icon(Icons.photo_library),
+                      title: new Text('Photo Library'),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  new ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Camera'),
+                    onTap: () {
+                      _imgFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: 280,
+          child: GestureDetector(
+            onTap: () => _showPicker(context),
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    color: _image == null ? Colors.grey : Colors.white,
+                    image: _image == null
+                        ? null
+                        : DecorationImage(image: FileImage(_image))),
+                child: _image == null
+                    ? Center(
+                        child: Text(
+                          "Nhấn để thêm hình nền",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      )
+                    : null),
+          ),
+        )
+      ],
+    );
   }
 }
 
 class Ingredient extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Text('b'),
+    );
   }
 }
 
 class Steps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Text('c'),
+    );
   }
 }
