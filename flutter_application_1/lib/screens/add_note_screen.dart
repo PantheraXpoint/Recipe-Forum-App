307e2725494d621242cde20bbae3c8258b048450
PantheraXpoint <nhatquang.panthera0207@gmail.com>
2021-06-22 21:14:09 +0700

    update add note screen

--- flutter_application_1/lib/screens/add_note_screen.dart
+++ flutter_application_1/lib/screens/add_note_screen.dart
@@ -1,1 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/components/constaints.dart';
+import 'package:flutter_application_2/model/note_db.dart';
+import 'package:flutter_application_2/model/notes.dart';
 
+class AddNoteScreen extends StatefulWidget {
+  @override
+  _AddNoteScreenState createState() => _AddNoteScreenState();
+}
+
+class _AddNoteScreenState extends State<AddNoteScreen> {
+  String title;
+  String body;
+  DateTime date;
+  TextEditingController titleController = TextEditingController();
+  TextEditingController bodyController = TextEditingController();
+  addNote(NoteModel note) {
+    DatabaseProvider.db.addNewNote(note);
+    print("note added successfully");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Add new note",
+          style: TextStyle(color: kSecondaryColor),
+        ),
+        backgroundColor: kPrimaryColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Note Title",
+              ),
+              style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: TextField(
+                controller: bodyController,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                    border: InputBorder.none, hintText: "your note"),
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            setState(() {
+              title = titleController.text;
+              body = bodyController.text;
+              date = DateTime.now();
+            });
+            NoteModel note =
+                NoteModel(title: title, body: body, creation_date: date);
+            addNote(note);
+            Navigator.pop(context);
+          },
+          label: Text("Save Note"),
+          icon: Icon(Icons.save)),
+    );
+  }
+}
